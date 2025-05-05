511148c19f462e20902db810072ca327d1bf9189
Nancia RAHARINIRINA <167851356+Rhren@users.noreply.github.com>
2025-05-05 14:25:24 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,58 +4,46 @@
 int main() 
 {
     MultiDimVector<int> vec;
-    // Tests avec différentes dimensions
-   // std::cout << "1D:" << std::endl;
-   // auto flat1 = vec.array({1, 2, 3});
-   // vec.print();
-    //std::cout << "ndim: " << flat1.ndim() << std::endl;
-   // flat1.ndim();
+    MultiDimVector<std::string> vec2;
 
-    //std::cout << "\n2D:" << std::endl;
-   // auto flat2 = vec.array({{1, 2}, {3, 4}});
-   // vec.print();
-    //std::cout << "ndim: " << flat2.ndim() << std::endl;
-   // flat2.ndim();
+   /* auto flat = vec2.array({"hello","you"});
+    auto stri = vec2.array({"coucou","hey"});
+    auto s = stri + flat;
+    s.print();
+    flat[1] = "bon";
+    flat.print();*/
 
     std::cout << "\n3D:" << std::endl;
     auto flat3 = vec.array({{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}});
    // std::cout << "ndim: " << flat3.ndim() << std::endl;
-    vec.print();
+    flat3.print();
     flat3.shape();
-    vec.print();
 
-        // Accès et affectation avec value()
-        std::cout << "\nAccès et affectation avec value():" << std::endl;
-        try
-        {
-            // Lecture
-            std::cout << "flat3.value({0, 1, 0}) = " << flat3.value({0, 1, 0}) << std::endl; // Affiche: 3
-            std::cout << "flat3.value({1, 0, 1}) = " << flat3.value({1, 0, 1}) << std::endl; // Affiche: 6
-            // Affectation
-            flat3.value({0, 1, 0}) = 10; // Change 3 en 10
-            flat3.value({1, 0, 1}) = 23; // Change 6 en 23
-            std::cout << "Après affectation:" << std::endl;
-            std::cout << "flat3.value({0, 1, 0}) = " << flat3.value({0, 1, 0}) << std::endl; // Affiche: 10
-            std::cout << "flat3.value({1, 0, 1}) = " << flat3.value({1, 0, 1}) << std::endl; // Affiche: 23
-            flat3.print(); // Affiche: array([[[1, 2], [10, 4]], [[5, 23], [7, 8]]])
-        }
-        catch(const std::out_of_range& e)
-        {
-            std::cout << "Erreur: " << e.what() << std::endl;
-        }
-        
+    auto flat1 = vec.array({{{0, 0}, {0, 0}}, {{0, 0}, {0, 1}}});
+    //auto sum = flat1 * flat3;
+    flat1[0][0][1] = flat3[0][0][1];
+    flat1.print();
 
-    //flat3 = vec.reshape(flat3,{7});
-    /* try
+   // sum.print();
+    /*try
     {
-        flat3 = vec.reshape(flat3, {7}); // Forme incompatible
-        flat3.shape();
+        // Lecture
+        std::cout << "flat3[0][1][0] = " << flat3[0][1][0] << std::endl; // Affiche: 3
+        std::cout << "flat3[1][0][1] = " << flat3[1][0][1] << std::endl; // Affiche: 6
+
+        // Affectation
+        flat3[0][1][0] = 10; // Change 3 en 10
+        flat3[1][0][1] = 23; // Change 6 en 23
+        std::cout << "Après affectation:" << std::endl;
+        std::cout << "flat3[0][1][0] = " << flat3[0][1][0] << std::endl; // Affiche: 10
+        std::cout << "flat3[1][0][1] = " << flat3[1][0][1] << std::endl; // Affiche: 23
+        flat3.print(); 
     }
-    catch (const std::invalid_argument& e)
+    catch(const std::out_of_range& e)
     {
         std::cout << "Erreur: " << e.what() << std::endl;
     }*/
-   
+    
     /*MultiDimVector<std::string> vec1;
     auto flat =  vec1.array({"Bonjour", "le", "monde"});
     vec1.print();*/
@@ -63,19 +51,6 @@ int main()
    /* std::cout << "\n4D:" << std::endl;
     auto flat4 = vec.array({{{{{1,2},{3,4}},{{5,6},{7,8}}},{{{9,10},{11,12}},{{13,14},{15,16}}}},{{{{17,18},{19,20}},{{21,22},{23,24}}},{{{25,26},{27,28}},{{29,30},{31,32}}}}});
     vec.print();*/
-
-    // Test avec une taille non uniforme (lève une exception)
-    /*try 
-    {
-        std::cout << "\n2D (invalide):" << std::endl;
-        auto flat_invalid = vec.array({{1, 2}, {3}});
-        for (const auto& val : flat_invalid) std::cout << val << " ";
-        std::cout << std::endl;
-    } 
-    catch (const std::exception& e) 
-    {
-        std::cout << "Erreur: " << e.what() << std::endl;
-    }*/
 
     return 0;
 }
